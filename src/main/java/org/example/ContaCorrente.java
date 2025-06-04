f03b7c7cdd28e95ab172298b51dee869f87e3a56
Samuel Erbet <samuelerbet@gmail.com>
2025-06-04 19:37:07 -0300

    adição arquivos

---
+++ src/main/java/org/example/ContaCorrente.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+
+}
---
+++ src/main/java/org/example/ContaPoupanca.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+    private double saldo;
+    private double taxa;
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+}
